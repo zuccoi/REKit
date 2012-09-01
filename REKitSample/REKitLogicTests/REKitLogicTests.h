d4023a23d13bf4bf8f47706fa1d3c3e26782f303
kazki <ili.o0o@me.com>
2012-09-01 15:04:35 +0900

    Added REKitLogicTests target.

---
+++ REKitSample/REKitLogicTests/REKitLogicTests.h
@@ -0,0 +1,13 @@
+//
+//  REKitLogicTests.h
+//  REKitLogicTests
+//
+//  Created by Kazki Miura on 9/1/12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface REKitLogicTests : SenTestCase
+
+@end
